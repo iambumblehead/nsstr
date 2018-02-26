e0fb88611e54a3f22b885fb87cb10487a3f5ded2
chris <chris@bumblehead.com>
2018-02-25 20:23:17 -0800

    small formatting changes

--- nsstr.js
+++ nsstr.js
@@ -1,34 +1,37 @@
-// Filename: nsstr.js  
+// Filename: nsstr.js
 // Timestamp: 2017.09.20-06:59:18 (last modified)
-// Author(s): bumblehead <chris@bumblehead.com>  
+// Author(s): bumblehead <chris@bumblehead.com>
 
 module.exports = (o => {
-
   o = opts =>
     o.parse(opts);
 
   o.ispathstr = str =>
     /^\.?\.?\//.test(str);
 
+  o.isfnsstr = str =>
+    /^\[[^\]]*\]\./.test(str);
+
   o.parsekeyprop = str =>
-    (str && str.match(/([^\.]*)\.(.*)/) || []).slice(1);
+    (str && str.match(/([^.]*)\.(.*)/) || []).slice(1);
 
   o.parsefnslns = str =>
     (str && str.match(/^(\[.*\]\.)?(.*)$/) || []).slice(1);
 
   o.parse = fullstr => {
-    let [fnsstr, nsstr] = o.parsefnslns(fullstr),
-        [nskey, nsprop] = o.parsekeyprop(nsstr),
-        fnskey, fnsprop,
+    let [ fnsstr, nsstr ] = o.parsefnslns(fullstr),
+        [ nskey, nsprop ] = o.parsekeyprop(nsstr),
+        fnskey,
+        fnsprop,
         fnspath;
 
     if (fnsstr) {
       fnsstr = fnsstr.substr(1, fnsstr.length - 3);
-      
+
       if (o.ispathstr(fnsstr)) {
         fnspath = fnsstr;
       } else {
-        [fnskey, fnsprop] = o.parsekeyprop(fnsstr);
+        [ fnskey, fnsprop ] = o.parsekeyprop(fnsstr);
       }
     }
 
@@ -38,7 +41,7 @@ module.exports = (o => {
       nsstr,   // local namespace, subj.type
       nskey,   // local key, base|subj|fkey|pkg|init
       nsprop,  // local prop, type
-      
+
       fnsstr,  // foreign namespace, 'fkey.articlekey' || '/dataerrors'
       fnspath, // foreign path, '/dataerrors'
       fnskey,  // foreign key, fkey
@@ -47,5 +50,4 @@ module.exports = (o => {
   };
 
   return o;
-
 })();
